6d98fad89e99ad80601df59837dcec5254f2919a
Koblan <40729254+Koblan@users.noreply.github.com>
2018-12-17 21:50:56 +0500

    Add files via upload

---
+++ Compass/js/main.js
@@ -0,0 +1,11 @@
+
+$('.rewiew__slider').slick({
+	dots: true,
+	infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+  $(".banner__arrow a").mPageScroll2id({
+  	offset : 85 ,
+  	scrollSpeed : 800
+  });

0a0532af0c4fad7d37efc35a6060bbfedf8cbde9
Koblan <40729254+Koblan@users.noreply.github.com>
2019-07-23 12:41:12 +0500

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,25 @@
+// var input = document.querySelector(".data__form-date");
+// var svg = document.querySelector(".date-svg");
+// var test = 0;
+// var test2 = 0;
+// var test3 = 1;
+// var svg = document.querySelector(".main__container .container");
+
+// var input2 = document.querySelector(".active");
+
+// function proverka(){
+// 	if( test == test2){
+// 		document.querySelector(".date-svg").classList.add('active');
+     
+// 	test2=test2+test3;
+	    
+// 	}
+// 	else{
+// 		document.querySelector(".date-svg").classList.remove('active');
+// 		test2=test2-test3;
+// 		test3=1;
+// 	};
+// }
+// input.onclick = function() {
+// 	proverka();
+	

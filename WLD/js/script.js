d867a52cdb975ca8feb07e3227067b9345f5c574
Koblan <40729254+Koblan@users.noreply.github.com>
2019-08-23 21:38:34 +0500

    Add files via upload

--- WLD/js/script.js
+++ WLD/js/script.js
@@ -1,15 +1,15 @@
        // Slider                                     
        var slides = document.querySelectorAll('#slides .slide');
        var currentSlide = 0;
-       
+
        function nextSlide() {
        	goToSlide(currentSlide+1);
        }
-       
+
        function previousSlide() {
        	goToSlide(currentSlide-1);
        }
-       
+
        function goToSlide(n) {
        	slides[currentSlide].className = 'slide';
        	currentSlide = (n+slides.length)%slides.length;
@@ -17,7 +17,7 @@
        }
        var next = document.getElementById('next');
        var previous = document.getElementById('prev');
-       
+
        next.onclick = function() {
        	nextSlide();
        };
@@ -36,140 +36,61 @@ hum.onclick = function() {
 	first.classList.toggle("first-mob");
 }
 
+/////////////////////MODAL/////////////////////////////////////////////
+var buttonCloseModal = document.querySelector(".modal__header-close");
+var button = document.querySelector(".open-modal");
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
 
-////// User Timing API /////////////////////
-performance.mark("startWork");
-doWork(); // Код для замера
-performance.mark("endWork");
+function openModal(){
+	backdrop.style.display = 'block';
+	modal.style.display = 'block';
+}
 
-var items = performance.getEntriesByType('mark');
-console.log(items)
+function closeModal() {
+	backdrop.style.display = 'none';
+	modal.style.display = 'none';
+}
 
-//////////lazy load///////////
-var LazyLoad = {
+button.onclick = openModal;
+buttonCloseModal.onclick = closeModal;
 
-	images: [],
+////////////////////////////////////ACCODIAN/////////////////////////////////////////////////////////////////////////////////////////////////
+var title = document.getElementsByClassName("accardion-title");
+var cont = document.getElementsByClassName("aaccardion-cont");
 
-	
 
-	init: function () {
-
-		this.initOnLoad();
-
-		this.initOnScroll();
-
-	},
-
-	
-
-	initOnLoad: function () {
-
-		var that = this;
-
-		$(window).on('load', function () {
-
-			that.prepareImagesCollection();
-
-			that.checkImages();
-
-		});
-
-		return false;
-
-	},
-
-	
-
-	prepareImagesCollection: function () {
-
-		var that = this;
-
-		
-
-		$('img[data-src]').each(function () {
-
-			var image = {
-
-				object: $(this),
-
-				offset: $(this).offset().top,
-
-				srcset: $(this).attr('data-srcset') ? $(this).attr('data-srcset') : null,
-
-				src: $(this).attr('data-src') ? $(this).attr('data-src') : null,
-
-				loaded: false
-
-			};
-
-			
-
-			that.images.push(image);
-
-		});
-
-		return false;
-
-	},
-
-	
-
-	checkImages: function () {
-
-		var that = this;
-
-		for (var i = 0; i < that.images.length; i++) {
-
-			var image = that.images[i];
-
-			if (image.loaded === false && image.offset < ($(window).scrollTop() + $(window).height() + 300)) {
-
-				that.loadImage(image);
-
-				image.loaded = true;
-
+for (var i = 0; i < title.length; i++) {
+	var curTitle = title[i];
+	title[i].addEventListener('click', function() {
+		var classes = this.getAttribute('class'),
+		newClasses = '',
+		classesArr = classes.split(' '),
+		newClassesArr = classes.split(' ');
+		for (var j = 0; j < classesArr.length; j++) {
+			if (classesArr[j] == 'active') {
+				classesArr.splice([j], 1);
 			}
-
+		}
+		if (classesArr.length === newClassesArr.length) {
+			classesArr.push('active');
+			newClasses = classesArr.join(' ');
+		} else {
+			newClasses = classesArr.join(' ');
 		}
 
-		return false;
+		for (var l = 0; l < title.length; l++) {
+			oldClasses = title[l].getAttribute('class');
+			oldClassesArr = oldClasses.split(' ');
+			for (var k = 0; k < oldClassesArr.length; k++) {
+				if (oldClassesArr[k] == 'active') {
+					oldClassesArr.splice([k], 1);
+				}
+				oldClasses = oldClassesArr.join(' ');
+				title[l].setAttribute('class', oldClasses);
 
-	},
-
-	
-
-	loadImage: function (image) {
-
-		image.object.attr('srcset', image.srcset)
-
-		.attr('src', image.src)
-
-		.removeAttr('data-srcset')
-
-		.removeAttr('data-src');
-
-		return false;
-
-	},
-
-	
-
-	initOnScroll: function () {
-
-		var that = this;
-
-		$(window).on('scroll', function () {
-
-			that.checkImages();
-
-		});
-
-		return false;
-
-	}
-
-};
-
-
-
-LazyLoad.init();
+			}
+		}
+		this.setAttribute('class', newClasses);
+	})
+}
